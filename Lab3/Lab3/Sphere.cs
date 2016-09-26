bfb64a9a3b830f0317d725d5c1bf554b09634fd1
David Lawlor <lawlor041988@gmail.com>
2016-09-26 01:16:44 +0100

    Labs

---
+++ Lab3/Lab3/Sphere.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Sphere : IHasVolume
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        public Sphere(string typeOfShape, double radius)
+        {
+            this.radius = radius;
+        }
+
+        public Sphere()
+            : this("", 0)
+        {
+
+        }
+
+        public double CalculateVolume()
+        {
+            return Math.PI * Math.Pow(radius, 3);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nThe radius of the sphere is " + radius + "\nThe volume of the sphere is " + CalculateVolume();
+        }
+    }
+}
+
